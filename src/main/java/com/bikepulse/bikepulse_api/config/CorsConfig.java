e0dc407ba5a50d5a50f527aaab4178a1c2254ad0
Pitonsio <ivaniglean@gmail.com>
2026-09-09 01:46:46 +0200

    Cors config corregido
    Se ha modificado CorsConfig para permitir que la web reciba información de nuestra BD

--- src/main/java/com/bikepulse/bikepulse_api/config/CorsConfig.java
+++ src/main/java/com/bikepulse/bikepulse_api/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.bikepulse.bikepulse_api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("http://localhost:4321") // Origenes permitidos
+                .allowedOrigins("http://localhost:4321", "https://bike-pulse-web.vercel.app") // Origenes permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Todos los encabezados permitidos
                 .maxAge(3600); // Tiempo de cache preflight en segundos
--- src/main/java/com/bikepulse/bikepulse_api/controller/Controller.java
+++ src/main/java/com/bikepulse/bikepulse_api/controller/Controller.java
@@ -2,7 +2,6 @@ package com.bikepulse.bikepulse_api.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ import com.bikepulse.bikepulse_api.stations.StationSummary;
 import utils.BikeEnum;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4321") // Permitimos que este puerto en concreto acceda a los datos
 public class Controller {
 	
 	private StationRepository stationRepo;
